77682c3952b1d2e6e878b983f3cfa73bebaea8ee
Brian Ressler <carlofabyss@hotmail.com>
2024-10-01 10:01:12 -0500

    Stop counter at zero if seconds is below threshold before second conversion

--- assets/js/uicontrol.js
+++ assets/js/uicontrol.js
@@ -582,24 +582,43 @@ document.addEventListener("DOMContentLoaded", function() {
 			minutes = Math.floor(seconds/60);
 			hours = Math.floor(minutes/60);
 			days = Math.floor(hours/24);
-			
-			hours = hours-(days*24);
-			minutes = minutes-(days*24*60)-(hours*60);
-			seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
 
-			if(tops_countdown_days != null && tops_countdown_hours != null && tops_countdown_minutes != null && tops_countdown_seconds != null) {
-				let days_objects = tops_countdown_days.getElementsByTagName("p");
-				let hours_objects = tops_countdown_hours.getElementsByTagName("p");
-				let minutes_objects = tops_countdown_minutes.getElementsByTagName("p");
-				let seconds_objects = tops_countdown_seconds.getElementsByTagName("p");
+			console.log(seconds);
 
-				if(days_objects.length == 2 && hours_objects.length == 2 && minutes_objects.length == 2 && seconds_objects.length == 2) {
-					days_objects[0].innerHTML = double_digit(days);
-					hours_objects[0].innerHTML = double_digit(hours);
-					minutes_objects[0].innerHTML = double_digit(minutes);
-					seconds_objects[0].innerHTML = double_digit(seconds);
+			if(seconds <= 0) {
+				if(tops_countdown_days != null && tops_countdown_hours != null && tops_countdown_minutes != null && tops_countdown_seconds != null) {
+					let days_objects = tops_countdown_days.getElementsByTagName("p");
+					let hours_objects = tops_countdown_hours.getElementsByTagName("p");
+					let minutes_objects = tops_countdown_minutes.getElementsByTagName("p");
+					let seconds_objects = tops_countdown_seconds.getElementsByTagName("p");
+
+					if(days_objects.length == 2 && hours_objects.length == 2 && minutes_objects.length == 2 && seconds_objects.length == 2) {
+						days_objects[0].innerHTML = double_digit(0);
+						hours_objects[0].innerHTML = double_digit(0);
+						minutes_objects[0].innerHTML = double_digit(0);
+						seconds_objects[0].innerHTML = double_digit(0);
+					}
+				}
+			} else {
+				hours = hours-(days*24);
+				minutes = minutes-(days*24*60)-(hours*60);
+				seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
+
+				if(tops_countdown_days != null && tops_countdown_hours != null && tops_countdown_minutes != null && tops_countdown_seconds != null) {
+					let days_objects = tops_countdown_days.getElementsByTagName("p");
+					let hours_objects = tops_countdown_hours.getElementsByTagName("p");
+					let minutes_objects = tops_countdown_minutes.getElementsByTagName("p");
+					let seconds_objects = tops_countdown_seconds.getElementsByTagName("p");
+
+					if(days_objects.length == 2 && hours_objects.length == 2 && minutes_objects.length == 2 && seconds_objects.length == 2) {
+						days_objects[0].innerHTML = double_digit(days);
+						hours_objects[0].innerHTML = double_digit(hours);
+						minutes_objects[0].innerHTML = double_digit(minutes);
+						seconds_objects[0].innerHTML = double_digit(seconds);
+					}
 				}
 			}
+			
 		},1000);
 	}
 
